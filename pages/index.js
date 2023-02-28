e72b505a3e116008a5438cbcf2a4d8653968c1e0
RAHUL-KAD <kadamrahulj0909@gmail.com>
2023-03-01 01:38:34 +0530

    removed logout option from dashboard

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,6 @@ export default function Home({ user }) {
   return (
     <>
       <p className="text-gray-700 text-3xl mb-16 font-bold mt-10">Dashboard</p>
-      {user.name} <br /> <br /> <br /> <br /> <br /> <br />
-      <Link href="/api/auth/logout">Logout</Link>
       <p className="text-gray-700 text-3xl mb-16 font-bold"></p>
       <div className="grid lg:grid-cols-3 gap-5 mb-16">
         <div className="rounded bg-pink-300 h-40 shadow-sm"></div>
