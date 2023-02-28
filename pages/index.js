cbd332c91b9977b9dbc3f9beea84bb49cece3cd7
RAHUL-KAD <kadamrahulj0909@gmail.com>
2023-02-28 15:50:34 +0530

    added padding

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,17 @@ import Link from "next/link";
 export default function Home({ user }) {
   return (
     <>
-      <p className="text-gray-700 text-3xl mb-16 font-bold">Dashboard</p>
+      <p className="text-gray-700 text-3xl mb-16 font-bold mt-10">Dashboard</p>
       {user.name} <br /> <br /> <br /> <br /> <br /> <br />
       <Link href="/api/auth/logout">Logout</Link>
       <p className="text-gray-700 text-3xl mb-16 font-bold"></p>
       <div className="grid lg:grid-cols-3 gap-5 mb-16">
-        <div className="rounded bg-white h-40 shadow-sm"></div>
+        <div className="rounded bg-pink-300 h-40 shadow-sm"></div>
         <div className="rounded bg-white h-40 shadow-sm"></div>
         <div className="rounded bg-white h-40 shadow-sm"></div>
       </div>
+      <div className="grid col-1 bg-white h-96 shadow-sm"></div>
+      <div className="grid col-1 bg-white h-96 shadow-sm"></div>
       <div className="grid col-1 bg-white h-96 shadow-sm"></div>
     </>
   );
--- pages/user.js
+++ pages/user.js
@@ -1,15 +1,33 @@
 import React from 'react'
 import { withPageAuthRequired } from '@auth0/nextjs-auth0'
+import { useState, useEffect } from "react"
 
-export default function getUserInfo({ user}) {
+export default function userInfo() {
+    const [userInfo, setUserInfo] = useState(null)
 
-    console.log(user)
+    useEffect(() => {
+        const getUserInfo = async () => {
+            try {
+                const response = await fetch("/api/auth/me")
+                const data = await response.json()
+                setUserInfo(data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getUserInfo()
+    }, [])
 
-  return (
-    <div>
-        {user.nickname}
-    </div>
-  )
+    return (
+        <div>
+            <h1>User Info</h1>
+            {userInfo && (
+                <div>
+                    <h2>{userInfo.name}</h2>
+                </div>
+            )}
+        </div>
+    )
 }
 
 export const getServerSideProps = withPageAuthRequired();
